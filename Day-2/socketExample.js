33eb3b8402928089627460269151bc1724b2c342
Abhishek Pandey <abhishekpandey.kr@gmail.com>
2017-06-14 15:53:10 +0530

    'ErrorHandling-added'

--- Day-2/socketExample.js
+++ Day-2/socketExample.js
@@ -11,8 +11,16 @@ const tcpServer = net.createServer(function (socket) {
   socket.on('data', function (data) {//example of the readable stream
     console.log('here is the readable stream', data);
     socket.write('server reply' + data)
+    //emitting an event, on each event throwing an error
+    socket.emit('error', new Error('forcefully injected error'))
+  });
+//handling each request using error handler!
+  socket.on('error', function (error) {
+    console.log('something went wrong here!');
   })
 })
+//to restrict he connection numbers
+tcpServer.maxConnections = 10;
 
 tcpServer.listen(function () {
   const port = tcpServer.address().port;
